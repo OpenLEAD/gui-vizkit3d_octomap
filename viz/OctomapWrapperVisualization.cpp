05291e4d96058eb5c2b2d8d24c20c9a511253ce6
alcantara09 <alcantara@poli.ufrj.br>
2014-05-19 13:03:32 -0300

    synching with GitHub

--- viz/OctomapWrapperVisualization.cpp
+++ viz/OctomapWrapperVisualization.cpp
@@ -1,43 +1,134 @@
 #include <iostream>
 #include "OctomapWrapperVisualization.hpp"
+#include <cstddef>
+
+#include <osg/PositionAttitudeTransform>
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/Point>
+#include <base/eigen.h>
+
+#include <osg/ShapeDrawable>
+
+#include <vizkit/Vizkit3DHelper.hpp>
 
 using namespace vizkit;
 
-struct OctomapWrapperVisualization::Data {
-    // Copy of the value given to updateDataIntern.
-    //
-    // Making a copy is required because of how OSG works
-    octomap_wrapper::OctomapWrapper data;
-};
+OctomapWrapperVisualization::OctomapWrapperVisualization() {
+	treeOrientation = Eigen::Quaterniond::Identity();
+	treePosition.setZero();
+	newmap = true;
+	colorize = false;
 
-
-OctomapWrapperVisualization::OctomapWrapperVisualization()
-    : p(new Data)
-{
+	vertices = new osg::Vec3Array();
+	std::cout << "constructor" << std::endl;
 }
 
-OctomapWrapperVisualization::~OctomapWrapperVisualization()
-{
-    delete p;
+OctomapWrapperVisualization::~OctomapWrapperVisualization() {
+	delete tree;
 }
 
-osg::ref_ptr<osg::Node> OctomapWrapperVisualization::createMainNode()
-{
-    // Geode is a common node used for vizkit plugins. It allows to display
-    // "arbitrary" geometries
-    return new osg::Geode();
+osg::ref_ptr<osg::Node> OctomapWrapperVisualization::createMainNode() {
+
+	std::cout << "creating main node" << std::endl;
+
+	transformNode = new osg::PositionAttitudeTransform();
+	treeNode = new osg::Geode();
+	transformNode->addChild(treeNode);
+
+	return transformNode;
+
 }
 
-void OctomapWrapperVisualization::updateMainNode ( osg::Node* node )
-{
-    osg::Geode* geode = static_cast<osg::Geode*>(node);
-    // Update the main node using the data in p->data
+void OctomapWrapperVisualization::updateMainNode(osg::Node* node) {
+	//conversion from wrapper to octomap
+
+	if (!newmap) {
+		treeNode->removeDrawable(treeNode->getDrawable(0));
+	}
+	newmap = false;
+
+	tree = octomap_wrapper::binaryMsgToMap(wrapper);
+
+	treeGeom = new osg::Geometry();
+
+	osg::Vec4 color_occ_thres = osg::Vec4(0.0f, 1.0f, 0.59f, 1.0f);
+	osg::Vec4 color_occ = osg::Vec4(0.0f, 0.59f, 0.59f, 1.0f);
+
+	osg::Vec4 color_emp_thres;
+	osg::Vec4 color_emp;
+
+	if (colorize) {
+		color_emp_thres = osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f);
+		color_emp = osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f);
+	} else {
+		osg::Vec4 color_emp_thres = osg::Vec4(1.0f, 0.0f, 1.0f, 0.1f);
+		osg::Vec4 color_emp = osg::Vec4(1.0f, 0.0f, 1.0f, 0.1f);
+	}
+
+	vertices->clear();
+	osg::ref_ptr < osg::Vec4Array > colors(new osg::Vec4Array());
+	for (octomap::OcTree::tree_iterator it = tree->begin_tree(0), end =
+			tree->end_tree(); it != end; ++it) {
+		if (it.isLeaf()) {
+			if (tree->isNodeOccupied(*it)) { // occupied voxels
+				if (tree->isNodeAtThreshold(*it)) {
+					vertices->push_back(
+							osg::Vec3(it.getCoordinate().x(),
+									it.getCoordinate().y(),
+									it.getCoordinate().z()));
+					// set a color
+					colors->push_back(color_occ_thres);
+				} else {
+					vertices->push_back(
+							osg::Vec3(it.getCoordinate().x(),
+									it.getCoordinate().y(),
+									it.getCoordinate().z()));
+					// set a color
+					colors->push_back(color_occ);
+
+				}
+			} else {
+				if (tree->isNodeAtThreshold(*it)) {
+					vertices->push_back(
+							osg::Vec3(it.getCoordinate().x(),
+									it.getCoordinate().y(),
+									it.getCoordinate().z()));
+					// set a color
+					colors->push_back(color_emp_thres);
+				} else {
+					vertices->push_back(
+							osg::Vec3(it.getCoordinate().x(),
+									it.getCoordinate().y(),
+									it.getCoordinate().z()));
+					// set a color
+					colors->push_back(color_emp);
+				}
+			}
+		}
+	}
+
+	treeGeom->setVertexArray(vertices.get());
+	treeGeom->setColorArray(colors.get());
+	treeGeom->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+
+	treeGeom->addPrimitiveSet(
+			new osg::DrawArrays(osg::PrimitiveSet::POINTS, 0,
+					vertices->size()));
+
+	treeNode->addDrawable(treeGeom.get());
+	osg::StateSet* state = treeGeom->getOrCreateStateSet();
+	state->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+
 }
 
-void OctomapWrapperVisualization::updateDataIntern(octomap_wrapper::OctomapWrapper const& value)
-{
-    p->data = value;
-    std::cout << "got new sample data" << std::endl;
+void OctomapWrapperVisualization::updateDataIntern(
+		octomap_wrapper::OctomapWrapper const& value) {
+
+	wrapper.binary = value.binary;
+	wrapper.id = value.id;
+	wrapper.resolution = value.resolution;
+	wrapper.data = value.data;
 }
 
 //Macro that makes this plugin loadable in ruby, this is optional.
--- viz/OctomapWrapperVisualization.hpp
+++ viz/OctomapWrapperVisualization.hpp
@@ -3,14 +3,21 @@
 
 #include <boost/noncopyable.hpp>
 #include <vizkit/Vizkit3DPlugin.hpp>
-#include <osg/Geode>
-#include <slam/octomap_wrapper/OctomapWrapper.hpp>
+
+#include <octomap_wrapper/OctomapWrapper.hpp>
+#include <octomap_wrapper/conversion.hpp>
+#include "octomap/OcTree.h"
+#include "octomap/AbstractOcTree.h"
+
+
+namespace osg {
+    class Geometry;
+}
 
 namespace vizkit
 {
     class OctomapWrapperVisualization
         : public vizkit::Vizkit3DPlugin<octomap_wrapper::OctomapWrapper>
-        , boost::noncopyable
     {
     Q_OBJECT
     public:
@@ -26,8 +33,24 @@ namespace vizkit
         virtual void updateDataIntern(octomap_wrapper::OctomapWrapper const& plan);
         
     private:
-        struct Data;
-        Data* p;
+
+        osg::ref_ptr<osg::Node> drawpoints();
+
+        octomap_wrapper::OctomapWrapper wrapper;
+        octomap::OcTree* tree;
+        Eigen::Vector3d treePosition;
+        Eigen::Quaterniond treeOrientation; // TODO acho q nao vou precisar
+        osg::ref_ptr< osg::PositionAttitudeTransform > transformNode;
+        osg::ref_ptr<osg::Geode> treeNode;
+        osg::ref_ptr<osg::Geometry> treeGeom;
+
+        osg::ref_ptr < osg::Vec3Array > vertices;
+        bool newmap;
+
+        bool colorize;
+        bool show_polygon;
+        double colorize_interval;   // 1/distance
+
     };
 }
 #endif
